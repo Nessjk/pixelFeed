ceedebb82f10caeb2d0295d22a0d9d2ff2718295
Tony Rositano <tony.rositano@pandauxstudio.com>
2018-10-27 10:26:36 +0200

    Version 0.1

--- public/js/components.js
+++ public/js/components.js
@@ -1,33 +1,4 @@
-// Define a new component called button-counter
-Vue.component('buttonCounter', {
-    props: {
-        icon: String,
-        title: String,
-        size: String
-       },
-    name: 'buttonCounter',
-    data: function () {
-      return {
-        count: 0
-      }
-    },
-    template: `
-    <div class="selectInput" size>
-        <div class="selectInput__content">
-        
-            <div class="icon">
-                <i>
-                <slot name="icon">
-                    {{icon}}
-                </slot>            
-                </i>
-            </div>
-            <div class="selectInput__title">
-                <slot name="title">
-                    <p> {{title}} </p>
-                </slot>
-            </div>
-        </div>
-    </div>`,
-
-  })
+var vid = document.getElementById("feedbackVid");
+vid.onplay = function() {
+    alert("The video has started to play");
+};
